185bc5290809be8b445b321ed2009045360bddd8
pembasmikuman <shazwan19@icloud.com>
2025-01-06 20:04:51 +0800

    some more

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,3 +1,48 @@
+// ========== CLOSE PROMO BAR =========
+const promoBar = document.getElementById('promo-bar');
+const promoCloseBtn = document.getElementById('close-promo-btn');
+const resetPromoBtn = document.getElementById('reset-promo-btn');
+
+promoBar.classList.remove('hidden');
+localStorage.removeItem('promo-bar-closed');
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Check if promo bar state is saved in localStorage
+    const isPromoClosed = localStorage.getItem('promo-bar-closed') === 'true';
+    
+    if (isPromoClosed) {
+        promoBar.classList.add('hidden');
+    }
+
+    // Close Promo Bar
+    promoCloseBtn.addEventListener('click', () => {
+        promoBar.classList.add('hidden');
+        localStorage.setItem('promo-bar-closed', 'true');
+    });
+});
+
+// ============= MENU OVERLAY TOGGLE =============
+function toggleSidePanel() {
+    const sidePanel = document.getElementById('side-panel');
+    const overlay = document.getElementById('overlay');
+    
+    sidePanel.classList.toggle('active');
+    overlay.classList.toggle('active');
+}
+
+// ============ SEARCH OVERLAY TOGGLE =============
+function toggleSearch() {
+    const searchOverlay = document.getElementById('search-overlay');
+    searchOverlay.classList.toggle('active');
+    
+    // Focus the search input when overlay opens
+    if (searchOverlay.classList.contains('active')) {
+        searchOverlay.querySelector('.mobile_search_container').focus();
+    }
+}
+
+
 // Select form, popup, and close button
 const contactForm = document.getElementById('contactForm');
 const thankYouPopup = document.getElementById('thankYouPopup');
