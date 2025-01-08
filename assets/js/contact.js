fc2f90aa80c431a65d68538679b4d2b9813ce7f0
pembasmikuman <shazwan19@icloud.com>
2025-01-08 16:06:18 +0800

    SOME MORE

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -15,3 +15,14 @@ contactForm.addEventListener('submit', (e) => {
 closePopupButton.addEventListener('click', () => {
     thankYouPopup.classList.add('hidden');
 });
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    reset: false // Animation repeat
+})
+  
+sr.reveal(`.main`, {transition: 500, origin: 'left', duration: 500})
+sr.reveal(`.footer_container`, {transition: 500, origin:'bottom', delay: 100})
--- assets/js/login.js
+++ assets/js/login.js
@@ -10,12 +10,17 @@ document.addEventListener('DOMContentLoaded', function() {
         e.preventDefault();
         loginForm.classList.add('hidden');
         signupForm.classList.remove('hidden');
+
+        sr.reveal(`.form`, {transition:1000, origin: left});
     });
 
     switchToLogin.addEventListener('click', function(e) {
         e.preventDefault();
         signupForm.classList.add('hidden');
         loginForm.classList.remove('hidden');
+
+        sr.reveal(`.form`, {transition:1000, origin: left});
+
     });
 
     signupForm.addEventListener('submit', (e) => {
@@ -32,12 +37,12 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-let sr = new ScrollReveal({
-    reset: false,
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
     distance: '60px',
-    duration: 1000,
-    delay: 400
-})
-
-
-sr.reveal(`.footer_container`, {transition: 500, origin:'bottom'})
+    duration: 2500,
+    reset: false // Animation repeat
+  })
+  
+sr.reveal(`.footer_container`, {transition: 500, origin:'bottom', delay: 100})
